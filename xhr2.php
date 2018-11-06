8ced29ba3fcc00d712ae8708d4cb8895e88db822
sungeyang <40833486+sungeyang@users.noreply.github.com>
2018-11-06 17:06:08 +0800

    Add files via upload

---
+++ xhr2.php
@@ -0,0 +1,19 @@
+<?PHP
+
+	//sleep(2);
+	//echo '{"name":"hello"}';
+	
+	//print_r($_FILES['file1']);
+	
+	//$index = $_GET['index'];
+	//sleep($index);
+	//echo $index;
+	
+	//header('Access-Control-Allow-Origin:*');
+	header('Access-Control-Allow-Origin:http://localhost');
+	header('Access-control-Allow-Methods:POST,GET');
+
+	
+	echo 'hello';
+
+?>
